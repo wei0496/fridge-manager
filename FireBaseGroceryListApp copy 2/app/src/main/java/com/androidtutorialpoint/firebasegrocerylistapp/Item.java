35eed5014ea873e777439b7973f9343369e8d8e4
wdqin <wdqin@bu.edu>
2017-11-25 17:21:23 -0500

    OCR activity added

---
+++ FireBaseGroceryListApp copy 2/app/src/main/java/com/androidtutorialpoint/firebasegrocerylistapp/Item.java
@@ -0,0 +1,13 @@
+package com.androidtutorialpoint.firebasegrocerylistapp;
+
+/**
+ * Created by 12118 on 2017/11/25.
+ */
+
+public class Item {
+    public int id;
+    public String name;
+    public String date;
+    public String tag1;
+    public String tag2;
+}
