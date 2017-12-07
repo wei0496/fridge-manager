762238d48c3bd4a3e60cf87361e447b32a786905
wei0496 <wei0496@bu.edu>
2017-12-07 12:31:56 -0500

    modify notifications - final - modify logic check to alarmreceiver

--- FireBaseGroceryListApp copy 2/app/src/main/java/com/androidtutorialpoint/firebasegrocerylistapp/AlarmReceiver.java
+++ FireBaseGroceryListApp copy 2/app/src/main/java/com/androidtutorialpoint/firebasegrocerylistapp/AlarmReceiver.java
@@ -11,33 +11,106 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    int flag = 1;
+    int flag = 0;
+    DatabaseReference mDB;
+    DatabaseReference mListItemRef;
+    private String Uid;
+    private Date Curr_Date;
+    public static final String TAG = "myLogs";
+    //Calendar cal = Calendar.getInstance();
+
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
+        Log.e(TAG, "Received");
         //check if any of our food are expiring here
 
-        if (flag == 1) {
-            Intent notificationIntent = new Intent(context, MenuActivity.class);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        Curr_Date = new Date();
+        //setContentView(R.layout.activity_main);
 
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-            stackBuilder.addParentStack(MenuActivity.class);
-            stackBuilder.addNextIntent(notificationIntent);
+        try {
+            Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-            PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            mDB = FirebaseDatabase.getInstance().getReference();
+            mListItemRef = mDB.child("listItem").child(Uid);
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            mListItemRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        ListItem listItem = child.getValue(ListItem.class);
+                        String ExpirationDuration = listItem.getExpirationDate();
+                        String CreationDate = listItem.getListItemCreationDate();
+                        String food = listItem.getName();
+                        Log.e(TAG, CreationDate);
+                        Calendar cal = Calendar.getInstance();
+                        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                        try {
+                            Date date = formatter.parse(CreationDate);
+                            //Log.e(TAG, formatter.format(date));
+                            cal.set(Calendar.YEAR, date.getYear());
+                            cal.set(Calendar.MONTH, date.getMonth());
+                            cal.set(Calendar.DAY_OF_MONTH, date.getDate());
+                            //Log.e(TAG, formatter1.format(cal));
+                        } catch (java.text.ParseException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        //set the expiration notification to be one day before the actual expiration
+                        cal.add(Calendar.HOUR_OF_DAY, 24 * (Integer.parseInt(ExpirationDuration)-1));
+                        cal.add(Calendar.YEAR, 1900);
+                        Date temp = cal.getTime();
+                        Log.e(TAG, "comparing...");
+                        Log.e(TAG, formatter.format(Curr_Date));
+                        Log.e(TAG, formatter.format(temp));
+                        if (temp.compareTo(Curr_Date) < 0) {
+                            Log.e(TAG, "expired!");
+                            flag = 1;
+                            Intent notificationIntent = new Intent(context, MenuActivity.class);
 
-            Notification notification = builder.setContentTitle("Magic Fridge Notification!!")
-                    .setContentText("New Notification From Magic Fridge!!")
-                    .setTicker("Your Food are expiring soon! -- Fridge Manager")
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentIntent(pendingIntent).build();
+                            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                            stackBuilder.addParentStack(MenuActivity.class);
+                            stackBuilder.addNextIntent(notificationIntent);
 
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0, notification);
+                            PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+                            Notification notification = builder.setContentTitle("Magic Fridge Notification!!")
+                                    .setContentText("Your " + food + " are expiring in one day! -- Fridge Manager!!")
+                                    .setTicker("Your Food are expiring soon! -- Fridge Manager")
+                                    .setSmallIcon(R.mipmap.ic_launcher)
+                                    .setContentIntent(pendingIntent).build();
+
+                            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                            notificationManager.notify(0, notification);
+                            break;
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError firebaseError) {
+                }
+            });
         }
+
+        catch (Exception e){}
     }
 }
+
--- FireBaseGroceryListApp copy 2/app/src/main/java/com/androidtutorialpoint/firebasegrocerylistapp/ExpirationActivity.java
+++ FireBaseGroceryListApp copy 2/app/src/main/java/com/androidtutorialpoint/firebasegrocerylistapp/ExpirationActivity.java
@@ -27,84 +27,34 @@ import java.util.Calendar;
 
 public class ExpirationActivity extends Activity {
 
-    boolean flag = false;
-    DatabaseReference mDB;
-    DatabaseReference mListItemRef;
-    private String Uid;
     private Date Curr_Date;
     public static final String TAG = "myLogs";
-    final Calendar cal = Calendar.getInstance();
-    final Calendar cal1 = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
-        Curr_Date = new Date();
+ Curr_Date = new Date();
         //setContentView(R.layout.activity_main);
-        try {
-            Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-            mDB = FirebaseDatabase.getInstance().getReference();
-            mListItemRef = mDB.child("listItem").child(Uid);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-            mListItemRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot child : dataSnapshot.getChildren()) {
-                        ListItem listItem = child.getValue(ListItem.class);
-                        String ExpirationDuration = listItem.getExpirationDate();
-                        String CreationDate = listItem.getListItemCreationDate();
-                        Log.e(TAG, CreationDate);
-                        DateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                        try{Date date = formatter.parse(CreationDate);
-                            cal.setTime(date);}
-                        catch (java.text.ParseException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        cal.add(Calendar.HOUR_OF_DAY, 24 * Integer.parseInt(ExpirationDuration));
-                        Date temp = cal.getTime();
-                        if (temp.compareTo(Curr_Date) > 0){
-                            flag = true;
-                            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent notificationIntent = new Intent("android.media.action.DISPLAY_NOTIFICATION");
+        notificationIntent.addCategory("android.intent.category.DEFAULT");
 
-                            Intent notificationIntent = new Intent("android.media.action.DISPLAY_NOTIFICATION");
-                            notificationIntent.addCategory("android.intent.category.DEFAULT");
+        PendingIntent broadcast = PendingIntent.getBroadcast(ExpirationActivity.this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-                            PendingIntent broadcast = PendingIntent.getBroadcast(ExpirationActivity.this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Set the alarm to start at 10:00 AM
+        // change the time below to set a different start time
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 1);
+        calendar.set(Calendar.MINUTE, 12);
+        calendar.set(Calendar.SECOND, 0);
 
-                            // Set the alarm to start at 10:00 AM
-                            // change the time below to set a different start time
-                            Calendar calendar = Calendar.getInstance();
-                            calendar.setTimeInMillis(System.currentTimeMillis());
-                            calendar.set(Calendar.HOUR_OF_DAY, 10);
-                            calendar.set(Calendar.MINUTE, 0);
-                            calendar.set(Calendar.SECOND, 0);
+        // The below code is for demo, triggered every 30 seconds
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 3000, broadcast);
+        Log.e(TAG, "Expires");
 
-                            // The below code is for demo, triggered every 30 seconds
-                            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 3000, broadcast);
-
-                            // comment the above code and uncomment the below code for actual use, triggered every 24 hours
-                            //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 86400000, broadcast);
-
-                            //Calendar cal1 = Calendar.getInstance();
-                            //For demo we set this to be called every 15 seconds
-                            //cal1.add(Calendar.SECOND, 15);
-                            //alarmManager.set(AlarmManager.RTC_WAKEUP, cal1.getTimeInMillis(), broadcast);
-                            Log.e(TAG, "Expires");
-                            break;
-                        }
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError firebaseError) {
-                }
-            });
-        }
-
-        catch (Exception e){}
         Intent intent = new Intent(ExpirationActivity.this, LoginActivity.class);
         startActivity(intent);
     }
